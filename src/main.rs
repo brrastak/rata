d6c21a4a8280f7b6642f0cd7f3186a0b19b1bfcb
Yuri Volkov <volkov.yuri.alex@gmail.com>
2023-08-13 09:46:31 +0200

    I2C test

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@
 
 use panic_rtt_target as _;
 use rtt_target::rtt_init_print;
+// use rtt_target::rprintln;
 use stm32f1xx_hal:: {
         prelude::*,
         gpio::*,
@@ -14,10 +15,13 @@ use stm32f1xx_hal:: {
         // afio::*,
         // timer::*,
         adc::*,
+        i2c::*,
     };
 use rtic_monotonics::systick::*;
 use usb_device::prelude::*;
 use usbd_serial::{SerialPort, USB_CLASS_CDC};
+use pcf857x::{Pcf8574, SlaveAddr};
+use mcp4725::*;
 
 
 
@@ -41,6 +45,7 @@ mod app {
         low2: Pin<'A', 9, Output>,
         heater_voltage: Pin<'B', 1, Analog>,
         adc: Adc<you_must_enable_the_rt_feature_for_the_pac_in_your_cargo_toml::ADC1>,
+        dac: MCP4725<BlockingI2c<you_must_enable_the_rt_feature_for_the_pac_in_your_cargo_toml::I2C2, (Pin<'B', 10, Alternate<OpenDrain>>, Pin<'B', 11, Alternate<OpenDrain>>)>>,
     }
 
     #[init]
@@ -50,7 +55,7 @@ mod app {
 
         let mut flash = cx.device.FLASH.constrain();
         let rcc = cx.device.RCC.constrain();
-        // let mut afio = cx.device.AFIO.constrain();
+        let mut afio = cx.device.AFIO.constrain();
  
         let clocks = rcc
             .cfgr
@@ -72,7 +77,7 @@ mod app {
         let mut gpioc = cx.device.GPIOC.split();
         
         let _heater_disable = gpiob.pb15.into_push_pull_output_with_state(&mut gpiob.crh, Low);
-        let _amp_enable = gpioa.pa1.into_push_pull_output_with_state(&mut gpioa.crl, Low);
+        let mut amp_enable = gpioa.pa1.into_open_drain_output_with_state(&mut gpioa.crl, Low);
         let beep = gpioc.pc13.into_push_pull_output_with_state(&mut gpioc.crh, Low);
         let switch = gpiob.pb7.into_floating_input(&mut gpiob.crl);
         let high1 = gpioa.pa8.into_push_pull_output_with_state(&mut gpioa.crh, High);
@@ -80,7 +85,11 @@ mod app {
         let high2 = gpiob.pb14.into_push_pull_output_with_state(&mut gpiob.crh, High);
         let low2 = gpioa.pa9.into_push_pull_output_with_state(&mut gpioa.crh, High);
         let heater_voltage = gpiob.pb1.into_analog(&mut gpiob.crl);
-
+        let i2c1_sck = gpiob.pb8.into_alternate_open_drain(&mut gpiob.crh);
+        let i2c1_sda = gpiob.pb9.into_alternate_open_drain(&mut gpiob.crh);
+        let i2c2_sck = gpiob.pb10.into_alternate_open_drain(&mut gpiob.crh);
+        let i2c2_sda = gpiob.pb11.into_alternate_open_drain(&mut gpiob.crh);
+        
 
         let mut delay = cx.device.TIM2.delay_us(&clocks);
         delay.delay_ms(500u32);
@@ -110,11 +119,53 @@ mod app {
         // channel.1.enable();
 
         let adc = Adc::adc1(cx.device.ADC1, clocks);
-   
+
+        let i2c = BlockingI2c::i2c1(
+            cx.device.I2C1,
+            (i2c1_sck, i2c1_sda),
+            &mut afio.mapr, 
+            Mode::Fast {
+                frequency: 400.kHz(),
+                duty_cycle: DutyCycle::Ratio16to9,
+            }, 
+            clocks,
+            1000,
+            10,
+            1000,
+            1000);
+
+        let mut vfd = Pcf8574::new(
+            i2c, 
+            SlaveAddr::default());
+        vfd.set(0xff).ok();
+        let i2c = vfd.destroy();
+
+        let mut vfd = Pcf8574::new(
+            i2c, 
+            SlaveAddr::Alternative(false, false, true));
+        vfd.set(0x00).ok();
+  
+        let audio_i2c = BlockingI2c::i2c2(
+            cx.device.I2C2,
+            (i2c2_sck, i2c2_sda),
+            Mode::Fast {
+                frequency: 400.kHz(),
+                duty_cycle: DutyCycle::Ratio16to9,
+            }, 
+            clocks,
+            1000,
+            10,
+            1000,
+            1000);
+        let mut dac = MCP4725::new(audio_i2c, 0b000);
+        dac.set_dac_fast(PowerDown::Normal, 0x000).ok();
+        amp_enable.set_high();
+
 
         beep::spawn().ok();
         // usb::spawn().ok();
         heater::spawn().ok();
+        audio::spawn().ok();
 
         (
             Shared {},
@@ -127,7 +178,8 @@ mod app {
                 high2,
                 low2,
                 heater_voltage,
-                adc
+                adc,
+                dac,
             },
         )
 
@@ -146,11 +198,10 @@ mod app {
         
     // }
 
-    #[task(local = [beep, switch, heater_voltage, adc], priority = 1)]
+    #[task(local = [beep, heater_voltage, adc], priority = 1)]
     async fn beep(cx: beep::Context) {
 
         let beep = cx.local.beep;
-        let switch = cx.local.switch;
         let heater_voltage = cx.local.heater_voltage;
         let adc = cx.local.adc;
 
@@ -158,28 +209,15 @@ mod app {
 
             let data: u16 = adc.read( heater_voltage).unwrap();
 
-            if data > 600 {
+            if data > 1300 {
 
                 beep.set_high();
                 Systick::delay(1.millis()).await;
             }
 
             beep.set_low();
-            Systick::delay(1000.millis()).await;
+            Systick::delay(2000.millis()).await;
         }
-        
-
-        // loop {
-        //     if switch.is_high() {
-        //         beep.set_low();
-        //     }
-        //     else {
-        //         beep.set_high();
-        //     }
-
-        //     Systick::delay(10.millis()).await;
-        // }
-        
     }
 
     #[task(local = [usb_bus], priority = 1)]
@@ -280,6 +318,28 @@ mod app {
             low1.set_high();
             high2.set_high();
             low2.set_high();
+        }
+    }
+
+    #[task(local = [switch, dac], priority = 1)]
+    async fn audio(cx: audio::Context) {
+
+        let switch = cx.local.switch;
+        let dac = cx.local.dac;
+
+        loop {
+
+            if switch.is_low() {
+
+                dac.set_dac_fast(PowerDown::Normal, 0x0f0).ok();
+                Systick::delay(1.millis()).await;
+
+                dac.set_dac_fast(PowerDown::Normal, 0x000).ok();
+                Systick::delay(1.millis()).await;
+            }
+            else {
+                Systick::delay(10.millis()).await;
+            }
         }
     }
 
